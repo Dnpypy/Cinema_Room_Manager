10eaac2a640e876a11a38068d9c2b52c128755ed
Dnpypy <denlatypoff@yandex.ru>
2021-10-27 15:12:42 +0300

    Stage 1/5: Arrangement

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -2,8 +2,28 @@ package cinema;
 
 public class Cinema {
 
+
+    private static final int ROW = 8;
+    private static final int COL = 8;
+
     public static void main(String[] args) {
-        // Write your code here
-        ....
+
+        String[][] matrixHall = new String[ROW][COL];
+        System.out.println("Cinema:");
+        for (int h = 1; h < 9; h++){
+
+            System.out.print(h + " ");
+        }
+
+        System.out.print("\n");
+
+        for (int i = 1; i < ROW; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < COL; j++) {
+                matrixHall[i][j] = "S";
+                System.out.print(matrixHall[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
