d2f939942c2ee26c4f4d91fa2d7d8dd4a7847d42
Dnpypy <denlatypoff@yandex.ru>
2021-11-03 17:37:36 +0300

    Two numbers never occur next to each other

--- Topics/Iterating over arrays/Two numbers never occur next to each other/src/Main.java
+++ Topics/Iterating over arrays/Two numbers never occur next to each other/src/Main.java
@@ -13,26 +13,29 @@ class Main {
         //1 2 3
         //2 3   false
         final int twoN = 2;
-        int[] arrT = new int[twoN];
+        int[] twoArr = new int[twoN];
 
         for (int i = 0; i < twoN; i++) {
-            arrT[i] = sc.nextInt();
+            twoArr[i] = sc.nextInt();
         }
 //        3
 //        1 2 3
 //        3 4  true
-        int oneNum = arrT[0];  // 3
-        int twoNum = arrT[1];  // 4
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] == oneNum && twoNum != arr[i + 1] && twoNum != arr[i - 1]) {
-               // System.out.println(arr[i]);
-                System.out.println(true);
-                break;
-            } else {
-                System.out.println(false);
+        int oneNum = twoArr[0];  // 3
+        int twoNum = twoArr[1];  // 4
+        int count = arr[0];
+        boolean tNumberChange = false;
+        for (int i = 0; i < arr.length - 1; i++) {
+            // 3 == 3 && 4 != 2 && 4 != 1
+            // 3 == 3 && 4 != 3 && 4 !=
+            //                       4 !=
+            if ((oneNum == arr[i] && twoNum != arr[i + 1] && twoNum != arr[i - 1]) ||
+               (twoNum == arr[i] && oneNum != arr[i + 1] && oneNum != arr[i - 1])){
+                tNumberChange = true;
                 break;
             }
         }
+        System.out.println(tNumberChange);
 
     }
 }
